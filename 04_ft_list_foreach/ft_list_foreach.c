803194477f5da2d56d86b5ddd0a0b879d40a9a0a
Bianca Behm <bbehm@c3r1p7.hive.fi>
2020-07-07 10:33:55 +0300

    ft_list_foreach

---
+++ 04_ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbehm <bbehm@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/03 11:40:50 by bbehm             #+#    #+#             */
+/*   Updated: 2020/07/07 09:46:08 by bbehm            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list *list_ptr;
+
+	list_ptr = begin_list;
+	while (list_ptr)
+	{
+		(*f)(list_ptr->data);
+		list_ptr = list_ptr->next;
+	}
+}
